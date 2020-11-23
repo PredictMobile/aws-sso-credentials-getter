29c777af8afbd10baacb2e0c65287efe40948cf0
Dennis H Knudsen <dkn@ambita.com>
2020-11-24 00:33:19 +0100

    Added possibilty to copy keys to a custom profile Profile dont have to exist to set creds

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,9 @@ const args = process.argv.splice(process.execArgv.length + 2)
 // Retrieve the first argument
 const profile = args[0]
 
+const customProfile = args[1] ? args[1] : undefined
+
 import SetCreds from "../lib/index.js"
 
 // Displays the text in the console
-SetCreds(profile)
+SetCreds(profile, customProfile)
